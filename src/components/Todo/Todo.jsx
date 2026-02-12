45bd817d51f49826e8d3d84543d8f0c9396a5667
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-12 22:51:43 +0530

    (chore) : add bindActionCreators

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,13 +1,14 @@
+import { bindActionCreators } from "redux";
 import { removeTodo } from "../../actions";
 import { useDispatch } from "react-redux";
 function Todo({title,id})
 {
     const dispatch=useDispatch();
-    
+    const actions=bindActionCreators({removeTodo},dispatch)
     return(
             <div>
                 {title}
-                <button onClick={()=>dispatch(removeTodo(id))}>Delete</button>
+                <button onClick={()=>actions.removeTodo(id)}>Delete</button>
             </div>
     )
 }
