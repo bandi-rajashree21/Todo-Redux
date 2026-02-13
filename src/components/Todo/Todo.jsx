60b8901f08124542f5e20c58b8135dc5c576a130
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-13 12:11:34 +0530

    (feat) : edit todo impl

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,15 +1,35 @@
 import { bindActionCreators } from "redux";
-import { removeTodo } from "../../actions";
+import { removeTodo ,editTodo } from "../../actions";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 function Todo({title,id})
 {
     const dispatch=useDispatch();
-    const actions=bindActionCreators({removeTodo},dispatch)
-    return(
-            <div>
-                {title}
-                <button onClick={()=>actions.removeTodo(id)}>Delete</button>
-            </div>
-    )
+    const actions=bindActionCreators({removeTodo,editTodo},dispatch)
+    const [isEditing,setIsEditing]=useState(false);
+    const [editedText,setEditedText]=useState(title);
+    function updateTodo()
+    {
+       if(isEditing)
+       {
+        actions.editTodo({id:id,title:editedText});
+        setIsEditing(false);
+       }
+       else{
+        setIsEditing(true);
+       }
+    }
+    return (
+      <div>
+        {isEditing ? 
+          <input
+            value={editedText}
+            onChange={(e) => setEditedText(e.target.value)}
+          />
+         :editedText }
+        <button onClick={() => actions.removeTodo(id)}>Delete</button>
+        <button onClick={updateTodo}>{(isEditing)?'Save':'Edit'}</button>
+      </div>
+    );
 }
 export default Todo;
